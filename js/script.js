99eea15e4436909263c1f6ff67f4c05c7d7ccaba
Samantha Simmons <sammysimmons274@gmail.com>
2020-02-08 18:40:39 +0000

    challenge 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //wait until page is ready
 $(document).ready(function() {
+
     //sets <a> element within paragraph  to yellow  
     $("p").click(function() {
         $(this).children("a").css("background-color", "yellow");
@@ -12,8 +13,16 @@ $(document).ready(function() {
         $(this).prev().slideToggle('slow');
     });
 
+
     // Open the paragraph once the image is clicked
     $("img").click(function() {
         $(this).next().children("p").slideDown();
+
+    });
+
+
+
+    $(".card").click(function() {
+        $(this).toggleClass("highlight");
     });
 });
